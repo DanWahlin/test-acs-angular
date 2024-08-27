fe7535d875616cb4df3562d3154e2e6df4f787b5
danwahlin <dwahlin@xmlforasp.net>
2024-08-27 00:10:59 -0700

    Initial checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CallClient } from "@azure/communication-calling";
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'test-angular';
+
+  async startCall() {
+    const callClient = new CallClient();
+    const tokenCredential = new AzureCommunicationTokenCredential('TOKEN');
+    let callAgent = await callClient.createCallAgent(tokenCredential);
+    let call = callAgent?.startCall(
+      [{ phoneNumber: 'PHONE_NUMBER_TO_CALL' }], {
+      alternateCallerId: { phoneNumber: 'PHONE_NUMBER_TO_CALL_FROM' }
+    });
+    console.log('Call id: ', call?.id);
+
+    // Adding event handlers to monitor call state
+    call?.on('stateChanged', () => {
+      console.log('Call state changed: ', call?.state);
+      if (call?.state === 'Disconnected') {
+        console.log('Call ended. Reason: ', call.callEndReason);
+      }
+    });
+  }
+
 }
